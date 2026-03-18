c7002076b040ea0f8bbb2a5650d6825fada6cfc0
angel-tomas <angelotomas.atgr@gamil.com>
2026-03-18 14:29:04 +0100

    HW 14.1. Views у Django

--- course/lesson_10/HW_10_1/django_project/books/urls.py
+++ course/lesson_10/HW_10_1/django_project/books/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import home, books, description
+from .views import (
+    HomePageTemplateView,
+    BooksListView,
+    BookDetailView,
+    BookCreateView,
+    ManageBookListView,
+    BookUpdateView,
+    BookDeleteView
+)
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('book/', books, name="books"),
-    path('book/<int:pk>/', description, name="book_detail"),
-
+    path('', HomePageTemplateView.as_view(), name="home"),
+    path('books', BooksListView.as_view(), name="books"),
+    path('books/<int:pk>/', BookDetailView.as_view(), name="book_detail"),
+    path('create_book/', BookCreateView.as_view(), name="create_book"),
+    path('manage_books/', ManageBookListView.as_view(), name="manage_books"),
+    path('manage_book/<int:pk>/update_book/', BookUpdateView.as_view(), name="update_book"),
+    path('manage_book/<int:pk>/delete_book/', BookDeleteView.as_view(), name="delete_book")
 ]
