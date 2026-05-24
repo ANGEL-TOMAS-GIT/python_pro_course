aeed5165138fbc63ff0ebcee0e03fa26445e89fe
angel-tomas <angelotomas.atgr@gamil.com>
2026-05-24 14:22:12 +0200

    HW 33.1. Deployment: Production Setup

--- course/homeworks/projects/django_project/payments/models.py
+++ course/homeworks/projects/django_project/payments/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model  # noqa: F401
 from django.core.validators import MinValueValidator
-from django.utils.translation import gettext_lazy as _
 from decimal import Decimal
 import uuid
 
--- course/homeworks/projects/django_project/payments/views.py
+++ course/homeworks/projects/django_project/payments/views.py
@@ -17,6 +17,8 @@ logger = logging.getLogger(__name__)
 
 STRIPE_P_KEY = settings.STRIPE_PUBLIC_KEY
 class CheckoutView(View):
+    
+    
     def get(self, request, order_id):
         order = get_object_or_404(Order, id=order_id)
 
